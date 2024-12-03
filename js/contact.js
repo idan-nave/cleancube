1e91f5ecdfccf9012dbd4203559e12a8f21b21d3
Amit Shitrit <amitgal21@gmail.com>
2024-12-03 22:10:12 +0200

    contact us created and finish the alg pages

---
+++ js/contact.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const contactForm = document.getElementById('contactForm');
+
+    contactForm.addEventListener('submit', (e) => {
+        e.preventDefault(); 
+
+        const formData = new FormData(contactForm);
+        const data = Object.fromEntries(formData.entries());
+
+        console.log('Form Submitted:', data);
+
+        alert('Your request has been submitted successfully!');
+        contactForm.reset(); 
+    });
+});
