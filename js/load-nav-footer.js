438062b3e111c36e02c5fe593be0f2dff3cdc755
Amit Shitrit <amitgal21@gmail.com>
2024-12-05 01:17:48 +0200

    footer and nav updated all pages changes zibby

--- js/load-nav-footer.js
+++ js/load-nav-footer.js
@@ -1,70 +1,83 @@
 document.addEventListener('DOMContentLoaded', () => {
     // nav content
     const navHTML = `
-        <nav class="global-nav">
-            <div class="nav-container">
-                <ul class="nav-links">
-                    <li>
-                        <a href="index.html">
-                            <img src="media/home.svg" alt="Home">
-                        </a>
-                    </li>
-                    <li>
-                        <a href="shop.html">
-                            <img id="shopBtn" src="media/shop.svg" alt="Our Store" >
-                        </a>
-                    </li>
-                    <li class="dropdown">
-                        <a href="#solutions" class="dropdown-toggle">
-                            <img src="media/algo.svg" alt="Cubics Algorithms">
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a href="./guides/2x2.html">2x2</a></li>
-                            <li><a href="./guides/3x3.html">3x3</a></li>
-                            <li><a href="./guides/4x4.html">4x4</a></li>
-                            <li><a href="./guides/pyraminx.html">Pyraminx</a></li>
-                            <li><a href="./guides/skewb.html">Skewb</a></li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="#cart">
-                            <img src="media/cart.svg" alt="My Cart">
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
+    <nav class="global-nav">
+        <div class="nav-container">
+            <ul class="nav-links">
+                <li>
+                    <a href="index.html">
+                        <img src="../media/home.svg" alt="Home">
+                        <span>Home</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="shop.html">
+                        <img id="shopBtn" src="../media/shop.svg" alt="Our Store">
+                        <span>Shop</span>
+                    </a>
+                </li>
+                <li class="dropdown">
+                    <a href="#solutions" class="dropdown-toggle">
+                        <img src="../media/algo.svg" alt="Cubics Algorithms">
+                        <span>Algorithms</span>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a href="./guides/2x2.html">2x2</a></li>
+                        <li><a href="./guides/3x3.html">3x3</a></li>
+                        <li><a href="./guides/4x4.html">4x4</a></li>
+                        <li><a href="./guides/pyraminx.html">Pyraminx</a></li>
+                        <li><a href="./guides/skewb.html">Skewb</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#cart">
+                        <img src="../media/cart.svg" alt="My Cart">
+                        <span>Cart</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+`;
 
-        `;
-
-
-    // footer content
-    const footerHTML = `
-        <footer class="global-footer">
-            <div class="footer-content">
+        const footerHTML = `
+    <footer class="global-footer">
+        <div class="footer-content">
             <div class="social-icons">
-            <a href="https://www.facebook.com/yourpage" target="_blank" class="icon">
-            <img src="./assests/footer/whatsapp.png" alt="WhatsApp">
-            </a>
-            <a href="https://www.facebook.com/yourpage" target="_blank" class="icon">
-            <img src="./assests/footer/facebook.png" alt="Facebook">
-            </a>
-            <a href="https://twitter.com/yourhandle" target="_blank" class="icon">
-            <img src="./assests/footer/twitter.png" alt="Twitter">
-            </a>
-            <a href="https://www.instagram.com/yourprofile" target="_blank" class="icon">
-            <img src="./assests/footer/instagram.png" alt="Instagram">
-            </a>
+                <h3>Follow Us</h3>
+                <div class="icon-row">
+                    <a href="https://www.facebook.com/yourpage" target="_blank" class="icon">
+                        <img src="../assests/footer/whatsapp.png" alt="WhatsApp">
+                    </a>
+                    <a href="https://www.facebook.com/yourpage" target="_blank" class="icon">
+                        <img src="../assests/footer/facebook.png" alt="Facebook">
+                    </a>
+                    <a href="https://twitter.com/yourhandle" target="_blank" class="icon">
+                        <img src="../assests/footer/twitter.png" alt="Twitter">
+                    </a>
+                    <a href="https://www.instagram.com/yourprofile" target="_blank" class="icon">
+                        <img src="../assests/footer/instagram.png" alt="Instagram">
+                    </a>
+                </div>
             </div>
             <ul class="footer-links">
-            <li><a href="#about">About</a></li>
-            <li><a href="#privacy">Privacy Policy</a></li>
-            <li><a href="../contactus.html">Contact</a></li>
+                <h3>Quick Links</h3>
+                <li><a href="#about">About</a></li>
+                <li><a href="#privacy">Privacy Policy</a></li>
+                <li><a href="../contactus.html">Contact</a></li>
             </ul>
-            <p>&copy; 2024 CleanCube. All rights reserved.</p>
+            <div class="footer-contact">
+                <h3>Contact</h3>
+                <p>Email: support@CleanCube.com</p>
+                <p>Phone: +123 456 7890</p>
+                <p>Address: David Ben Gurion 7 TLV</p>
             </div>
-        </footer>
-    `;
+        </div>
+        <p class="footer-bottom">&copy; 2024 CleanCube. All rights reserved.</p>
+    </footer>
+`;
+
+    
 
     const navPlaceholder = document.getElementById('nav-placeholder');
     if (navPlaceholder) {
--- js/product.js
+++ js/product.js
@@ -1,10 +1,29 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const mainImage = document.getElementById("main-image");
-    const thumbnails = document.querySelectorAll(".thumbnail-images img");
+document.addEventListener('DOMContentLoaded', () => {
+    function preloadImages() {
+        const images = [
+            '../assests/shop/2x2-img/2x2-img1.jpeg',
+            '../assests/shop/2x2-img/2x2-img2.jpeg',
+            '../assests/shop/2x2-img/2x2-img3.jpeg',
+            '../assests/shop/2x2-img/2x2-img4.jpeg',
+            '../assests/shop/2x2-img/2x2-img5.jpeg',
+            '../assests/shop/2x2-img/2x2-img6.jpeg'
+        ];
+        images.forEach(src => {
+            const img = new Image();
+            img.src = src;
+        });
+    }
 
-    thumbnails.forEach((thumbnail) => {
-        thumbnail.addEventListener("mouseover", function () {
-            mainImage.src = this.getAttribute("data-src");
+
+    const thumbnails = document.querySelectorAll('.thumbnail-images img');
+    const mainImage = document.getElementById('main-image');
+
+    thumbnails.forEach(thumbnail => {
+        thumbnail.addEventListener('mouseenter', () => {
+            mainImage.src = thumbnail.src; 
         });
     });
+
+
+    preloadImages();
 });
